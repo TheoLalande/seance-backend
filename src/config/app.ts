f056c4d4235a8cc5405ac67b3d45dc3084663a27
a.vinsonneau <antoine.vinsonneau@compufirst.com>
2023-01-26 11:12:00 +0100

    json express

--- src/config/app.ts
+++ src/config/app.ts
@@ -4,7 +4,7 @@ import { myRouter } from "../routes/router";
 import * as bodyParser from "body-parser";
 
 const app: Application = express();
-
+app.use(express.json());
 app.use(bodyParser.json({ limit: "50mb", type: "application/vnd.api+json" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.get("/", (req: Request, res: Response) => {
