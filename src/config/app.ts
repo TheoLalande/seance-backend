93241b48cd7851600f5fa60aeb51047f2505050b
a.vinsonneau <antoine.vinsonneau@compufirst.com>
2023-01-26 11:12:33 +0100

    json express

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,12 +1,12 @@
 import * as express from "express";
 import { Application, Request, Response } from "express";
 import { myRouter } from "../routes/router";
-import * as bodyParser from "body-parser";
+// import * as bodyParser from "body-parser";
 
 const app: Application = express();
 app.use(express.json());
-app.use(bodyParser.json({ limit: "50mb", type: "application/vnd.api+json" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+// app.use(bodyParser.json({ limit: "50mb", type: "application/vnd.api+json" }));
+// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello mec");
 });
