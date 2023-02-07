67101d1a5f53d40aa549303a072b370191fe1c6a
AntoineP2 <antoine.vinsonneau34@gmail.com>
2023-02-07 08:58:42 +0100

    sqlite3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from "./config/app";
 
 app.listen(3000, () => {
-  console.log("Serveur Express With TS is runing on port 3000");
+  console.log("Serveur Express With TS is runing on port 3000, Ping with http://localhost:3000/ for test");
 });
