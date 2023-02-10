c118030cbb72b8227dc58ab33339ac3a49f766cc
a.vinsonneau <antoine.vinsonneau@compufirst.com>
2023-02-10 11:32:24 +0100

    bonne base

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,13 +1,12 @@
-import * as sqlite3 from 'sqlite3'
+import * as sqlite3 from "sqlite3";
 // import * as bodyParser from "body-parser";
 
-
-const dbname = 'mspr.db'
-const db = new sqlite3.Database(dbname, err => {
+const dbname = "mspr.sqlite";
+const db = new sqlite3.Database(dbname, (err) => {
   if (err) {
-    throw err
+    throw err;
   }
-  console.log("Connecté à", dbname)
-})
+  console.log("Connecté à", dbname);
+});
 
-export default db
+export default db;
