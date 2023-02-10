50d27d9c90eebddd01db9bb31728f2a84c22bfca
a.vinsonneau <antoine.vinsonneau@compufirst.com>
2023-02-10 08:57:03 +0100

    database

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import app from "./config/app";
 
-app.listen(3000, () => {
-  console.log("Serveur Express With TS is runing on port 3000, Ping with http://localhost:3000/ for test");
+app.listen(3005, () => {
+  console.log(
+    "Serveur Express With TS is runing on port 3000, Ping with http://localhost:3005/ for test"
+  );
 });
