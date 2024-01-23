b9c0657336cf2d1481cb93ff33b5cad4167e4010
theo.lalande <theo.lalande@compufirst.com>
2024-01-23 12:20:36 +0100

    ajout bdd

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import app from "./config/app";
-const server: number = 3002
-app.listen(server, "10.34.4.159", () => {
-  console.log(`Le serveur est lancé sur http://10.34.4.159:${server} le wifi local`);
+app.listen(3002, "localhost", () => {
+  console.log(`Le serveur est lancé sur http://localhost le wifi local`);
 });
